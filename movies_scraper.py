c2d2abfa1e18d2ad97a58e43dafe0ea6d6b834c3
mdiskbot200 <123540824+mdiskbot200@users.noreply.github.com>
2023-01-25 17:52:42 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "Your Api Key"
+api_key = "2f0c6bee42fd4d32783a91b33093d042e8108a7a"
 
 
 def search_movies(query):
@@ -34,7 +34,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://shortnerfly.com/api?api={api_key}&url={i['href']}"
+            url = f"https://omegalinks.in/st?api=2f0c6bee42fd4d32783a91b33093d042e8108a7a&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
